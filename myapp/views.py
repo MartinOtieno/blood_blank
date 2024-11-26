ea50895a69209e671e664a3ac7f8986f7ea60749
monyango21@student.kibu.ac.ke <monyango21@student.kibu.ac.ke>
2024-11-26 21:09:26 +0300

    changes on the app

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+#my landing page
+def index_page(request):
+    """ Display the home page """
+    return render(request, "index.html")
